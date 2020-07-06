240881c9ed06eecb7d4e6630cd752f2106146cd3
liwy <weiyong.li@beantechs.com>
2020-07-07 01:00:52 +0800

    配置导入

---
+++ gulimall-mamber/src/main/java/com/lwy/gulimall/mamber/GulimallMamberApplication.java
@@ -0,0 +1,13 @@
+package com.lwy.gulimall.mamber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallMamberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallMamberApplication.class, args);
+    }
+
+}
---
+++ gulimall-mamber/src/test/java/com/lwy/gulimall/mamber/GulimallMamberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwy.gulimall.mamber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMamberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
